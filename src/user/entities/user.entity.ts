398f41a351922b35f0f77935464b8924fa10204f
Nikolay <nikolay.tyshchenko04@gmail.com>
2024-07-04 18:21:36 +0300

    Connected the PostgreSQL + TypeORM database. Configured the relationship between the tables

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,38 @@
+import { Category } from 'src/category/entities/category.entity';
+import { Transaction } from 'src/transaction/entities/transaction.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Transaction, (transaction) => transaction.user, {
+    onDelete: 'CASCADE',
+  })
+  transaction: Transaction[];
+
+  @OneToMany(() => Category, (category) => category.user, {
+    onDelete: 'CASCADE',
+  })
+  categories: Category[];
+}
